a9309be66907ebb70f6996324f7907ea4e09f6bd
KeisukeHomma <ab13891012@gmail.com>
2021-05-20 23:02:41 +0900

    feature: スタイル実装準備

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/pages/index.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <button>あいうえお</button>
+    </div>
+  );
+}
